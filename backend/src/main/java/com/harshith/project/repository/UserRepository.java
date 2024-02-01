67e2d2a407e0a013c5039a54bdef2f251ece20ab
harshithrao07 <harshithrao316@gmail.com>
2024-02-01 16:30:35 +0530

    commitment

--- backend/src/main/java/com/harshith/project/repository/UserRepository.java
+++ backend/src/main/java/com/harshith/project/repository/UserRepository.java
@@ -13,6 +13,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.Objects;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -34,22 +36,35 @@ public interface UserRepository extends JpaRepository<User, Integer> {
                     .password(passwordEncoder.encode(request.getPassword()))
                     .role(Role.USER)
                     .build();
+            Optional<User> optionalUser = repository.findByEmail(request.getEmail());
+            if(optionalUser.isPresent()) {
+                return AuthenticationResponse.builder().message("Email address is already registered").build();
+            }
             repository.save(user);
             var jwtToken = jwtService.generateToken(user);
             return AuthenticationResponse.builder().token(jwtToken).build();
         }
 
         public AuthenticationResponse authenticate(AuthenticateRequest request) {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            request.getEmail(),
-                            request.getPassword()
-                    )
-            );
-            var user = repository.findByEmail(request.getEmail())
-                    .orElseThrow();
-            var jwtToken = jwtService.generateToken(user);
-            return AuthenticationResponse.builder().token(jwtToken).build();
+            try {
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getEmail(),
+                                request.getPassword()
+                        )
+                );
+                var user = repository.findByEmail(request.getEmail())
+                        .orElseThrow();
+
+                if(passwordEncoder.matches(request.getPassword(), user.getPassword())){
+                    var jwtToken = jwtService.generateToken(user);
+                    return AuthenticationResponse.builder().token(jwtToken).build();
+                } else {
+                    return AuthenticationResponse.builder().message("Password entered is incorrect.").build();
+                }
+            } catch (Exception e) {
+                return AuthenticationResponse.builder().message("Authentication failed.").build();
+            }
         }
     }
 }
--- backend/src/main/java/com/harshith/project/service/JwtService.java
+++ backend/src/main/java/com/harshith/project/service/JwtService.java
@@ -59,7 +59,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 300000)) // 5 minutes
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
